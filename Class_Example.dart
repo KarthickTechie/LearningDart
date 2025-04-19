3a32ab8b2c79bce6776824a2c18721cf26c99f3a
KarthickTechie <reachkarthickprogramer@gmail.com>
2025-04-19 19:03:53 +0530

    class examples

---
+++ Class_Example.dart
@@ -0,0 +1,47 @@
+class Person {
+  static final app_copy_right = "All Rights Reserved";
+  final String _name;
+
+  // Generative Constructor
+
+  const Person(this._name);
+
+  String greet(String host) => "hello $host , I am $_name";
+
+  static UpperCase() {
+    return app_copy_right.toUpperCase();
+  }
+}
+
+class Imposter implements Person {
+  String __name = ""; // Add a backing field for _name
+  DateTime? dob;
+
+  Imposter(this.dob);
+
+  @override
+  String get _name => __name; // Getter returns the backing field
+
+  @override
+  set _name(String somename) {
+    __name = somename; // Setter updates the backing field
+  }
+
+  @override
+  String greet(String host) {
+    print(Person.UpperCase());
+    return "hello $host , Do you know $_name , he was born on $dob";
+  }
+}
+
+void main() {
+  print(Person("Karthick").greet("Kevin"));
+  Imposter imp = Imposter(DateTime.now());
+  imp._name = "Akash";
+  print(imp.greet("Vikram"));
+
+  // const constructor
+
+  var personConstant = const Person("ConstantPerson");
+  print("from constant constructor => \"${personConstant._name}\"");
+}
---
+++ VoterID_Generator.dart
@@ -0,0 +1,93 @@
+/* 
+Voter ID generator clas 
+expect a list of VoterIDApplicant List 
+
+class VoterIDApplicant 
+
+typedef VoterIDApplicant = Map<String,>
+ */
+
+import 'dart:math';
+
+List<String> alphabets = const [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'x',
+  'y',
+  'z',
+];
+
+class Applicant {
+  String name;
+  String dob;
+  int? id;
+  int? age;
+  String? voterId;
+
+  Applicant(this.name, this.dob) {
+    this.age = this.calculateAge();
+    this.id = this.generateId();
+    this.voterId = this.generateVoterID();
+  }
+
+  int calculateAge() {
+    return DateTime.now().year - DateTime.parse(this.dob).year;
+  }
+
+  int generateId() {
+    return new Random().nextInt(1000000000);
+  }
+
+  String generateRandomChars() {
+    String hash = "";
+    final first3chars = this.id!.toString().substring(0, 3).split('');
+    first3chars.forEach((c) {
+      hash += alphabets[int.parse(c)];
+    });
+    return hash;
+  }
+
+  String? generateVoterID() {
+    if (this.age! >= 18) {
+      String hash = generateRandomChars();
+      final remainingchars = this.id!.toString().substring(
+        3,
+        this.id!.toString().length,
+      );
+      return "${hash.toUpperCase()}$remainingchars";
+    } else {
+      return null;
+    }
+  }
+
+  @override
+  String toString() {
+    return """Applicant [ Name : ${this.name} ID : ${this.id} DOB : ${this.dob} 
+    AGE : ${this.age} VOTERID : ${this.voterId} ]""";
+  }
+}
+
+void main() {
+  var applicant1 = Applicant("Karthick", "1991-12-12");
+  print(applicant1.toString());
+}
